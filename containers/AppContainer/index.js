87bb667f148031d8c630388782f36ddde7b27c85
willie <williepart3@gmail.com>
2020-04-17 21:37:34 +0300

    - splitted out passwordless related variables

--- containers/AppContainer/index.js
+++ containers/AppContainer/index.js
@@ -1,17 +1,12 @@
-import React, {useContext, useEffect} from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import React, { useContext } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import {routes} from '../../constants';
-import {SignUpScreen} from '../../screens/signUp';
-import {SignInScreen} from '../../screens/signIn';
-import {CodeFillScreen} from '../../screens/codeFill';
-import {HomeScreen} from '../../screens/home';
+import { routes } from '../../constants';
+import { SignInScreen } from '../../screens/signIn';
+import { CodeFillScreen } from '../../screens/codeFill';
+import { HomeScreen } from '../../screens/home';
 import ProfileScreen from '../../screens/profile';
-import SSOScreen from '../../screens/sso';
-import {NativeAuthContext} from '../../sh-universal-user-auth/native';
-import Loader from '../../components/loader';
-import {Alert} from 'react-native';
-import withAuth from '../../components/withAuth';
+import { NativeAuthContext } from '../../sh-universal-user-auth/native';
 
 const {Navigator, Screen} = createStackNavigator();
 
